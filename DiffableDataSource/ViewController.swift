4ab3a8843782b3952e2e7111f3a83393b8bfaf9b
krentz <r4f4_kr3ntz@hotmail.com>
2020-07-22 00:40:20 +0100

    first test with diffable data source

--- DiffableDataSource/ViewController.swift
+++ DiffableDataSource/ViewController.swift
@@ -10,11 +10,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tblShow: UITableView!
+    
+    let arrItems = [EmpModel(id: "1", name: "Tushar"),EmpModel(id: "2", name: "Anshul"),EmpModel(id: "3", name: "Ayush"),EmpModel(id: "4", name: "Prajakta")]
+    var dataSource: UITableViewDiffableDataSource<Section, EmpModel>! = nil
+    var currentSnapshot: NSDiffableDataSourceSnapshot<Section, EmpModel>! = nil
+    let identifer = "cell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tblShow.register(UITableViewCell.self, forCellReuseIdentifier: identifer)
+        configureDataSource()
+        updateUI()
     }
 
-
+    func configureDataSource() {
+        
+        self.dataSource = UITableViewDiffableDataSource
+            <Section, EmpModel>(tableView: tblShow) { [weak self]
+                (tableView: UITableView, indexPath: IndexPath, item: EmpModel) -> UITableViewCell? in
+                
+                let cell = tableView.dequeueReusableCell(
+                    withIdentifier: self!.identifer,
+                    for: indexPath)
+                cell.textLabel?.text = item.name
+                cell.detailTextLabel?.text = item.id
+                
+                return cell
+        }
+        self.dataSource.defaultRowAnimation = .fade
+        
+    }
+    func updateUI(animated: Bool = true) {
+          currentSnapshot = NSDiffableDataSourceSnapshot<Section, EmpModel>()
+          currentSnapshot.appendSections([.main])
+          currentSnapshot.appendItems(arrItems, toSection: .main)
+          self.dataSource.apply(currentSnapshot, animatingDifferences: animated)
+      }
 }
 
+
+enum Section: CaseIterable {
+    case main
+}
+
+struct EmpModel:Hashable{
+    var id: String
+    var name:String
+    init(id:String,name:String) {
+        self.id = id
+        self.name = name
+    }
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(self.id)
+    }
+}
